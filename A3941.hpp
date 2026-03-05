fb7f4855a8973f7e097b93a7eba9c8a978a22465
Danilo Fróes <danilofroes.20241@poli.ufrj.br>
2026-03-05 03:23:14 -0300

    REFACTOR: Refazendo o código sem a biblioteca do arduino, apenas esp-idf
    otimização de velocidade para o código

--- A3941.hpp
+++ A3941.hpp
@@ -4,30 +4,117 @@ Cabeçalho responsável pelo controle do driver de motor A3941.
 
 #pragma once
 
-#include <Arduino.h>
+#include "driver/ledc.h"
+#include "driver/gpio.h"
+
+// Namespace para as constantes que serão usadas na classe
+namespace ConstA3941 {
+    // Frequência recomendada para motores de potência para controlar melhor a comutação dos MOSFETs
+    constexpr uint32_t FREQUENCIA_SINAL_PWM = 20000;
+
+    // Resolução do PWM no valor convencional
+    constexpr ledc_timer_bit_t RESOLUCAO_SINAL_PWM = LEDC_TIMER_8_BIT;
+}
 
 // Classe para controlar motores que utilizam o driver A3941
 class A3941 {
-    private:
+private:
 
-    // Atributo que armazena o pino PHASE do driver de motor
-    uint8_t _pinoPHASE;
+    // Atributo para armazenar os pinos
+    gpio_num_t _pinoPHASE;
+    gpio_num_t _pinoPWML;
+    gpio_num_t _pinoPWMH;
 
     // Atributo que armazena o canal utilizado pelo pino PWMH do driver de motor
     // Esse canal é responsável por gerar o sinal PWM
-    uint8_t _canalDoPinoPWMH;
+    ledc_channel_t _canalDoPinoPWMH;
+
+    // Atributos para armazenar as configurações do timer e canal de PWM
+    ledc_mode_t modoPWM;
+    ledc_timer_t timerSelecionado;
 
     // Atributo que armazena o número de bits da resolução do sinal PWM
-    uint8_t _resolucao;
+    ledc_timer_bit_t resolucao;
 
-    // Calcula o valor máximo de potência do motor a partir da resolução
-    uint32_t _valorMaximoDePotencia = (1 << _resolucao) - 1;
+    void setCanalPWM(uint8_t canalPinoPWMH) {
+        #ifdef SOC_LEDC_SUPPORT_HS_MODE
+            if (canalPinoPWMH <= 7) {
+                this->_canalDoPinoPWMH = static_cast<ledc_channel_t>(canalPinoPWMH);
+                this->modoPWM = LEDC_LOW_SPEED_MODE;
+            }
 
-    // Atributo que armazena o valor da potêncial atual do motor
-    int _potencia = 0;
+            else if (canalPinoPWMH > 7 && canalPinoPWMH <= 15) {
+                // Transforma os canais de 8 a 15 em valores de 0 a 7 para a função de inicialização
+                this->_canalDoPinoPWMH = static_cast<ledc_channel_t>(canalPinoPWMH - 8);
+                // Os canais de 8 a 15 são do hardware HIGH SPEED
+                this->modoPWM = LEDC_HIGH_SPEED_MODE;
+            }
+        #else
+            this->_canalDoPinoPWMH = static_cast<ledc_channel_t>(canalPinoPWMH % 8);
+            this->modoPWM = LEDC_LOW_SPEED_MODE;
+        #endif
+    }
 
-    public:
+    void initCanalPWM() {
+        bool timerAlocado = false;
 
+        // Inicia um loop para verificar os timers disponíveis e escolher um para configurar o canal do driver
+        for (uint8_t i = 0; i < LEDC_TIMER_MAX; i++) {
+            ledc_timer_t timerTeste = static_cast<ledc_timer_t>(i);
+
+            uint32_t freqAtual = ledc_get_freq(modoPWM, timerTeste); // Salva a frequência do timer de teste
+
+            // Se a frequência do timer de teste já for igual a um timer em funcionamento, então esse timer será reutilizado
+            if (freqAtual == ConstA3941::FREQUENCIA_SINAL_PWM) {
+                timerSelecionado = timerTeste;
+                timerAlocado = true;
+                break; // quebra o loop
+            }
+
+            // Se a frequência do timer de teste for 0, significa que ele está livre para ser configurado
+            else if (freqAtual == 0) {
+                timerSelecionado = timerTeste;
+
+                ledc_timer_config_t timerConfig = {
+                    .speed_mode = modoPWM, // Escolhe o modo de velocidade de acordo com o canal de entrada na classe
+                    .duty_resolution = resolucao,
+                    .timer_num = timerSelecionado,
+                    .freq_hz = ConstA3941::FREQUENCIA_SINAL_PWM,
+                    .clk_cfg = LEDC_AUTO_CLK
+                };
+
+                ledc_timer_config(&timerConfig);
+                timerAlocado = true;
+                break; // quebra o loop
+            }
+        }
+
+        ledc_channel_config_t canalConfig = {
+            .gpio_num = _pinoPWMH, // Pino de saída
+            .speed_mode = modoPWM, // Escolhe o modo de velocidade de acordo com o canal de entrada na classe
+            .channel = _canalDoPinoPWMH, // Canal já remapeado
+            .intr_type = LEDC_INTR_DISABLE, // desativa as interrupções
+            .timer_sel = timerSelecionado, // Timer já configurado
+            .duty = 0, // Começa com o duty cicle em 0
+            .hpoint = 0
+        };
+
+        ledc_channel_config(&canalConfig);
+    }
+
+    void initGPIO() {
+        gpio_config_t gpioConfig = {
+            .pin_bit_mask = (1ULL << _pinoPWML) | (1ULL << _pinoPHASE), // configura os pinos juntos
+            .mode = GPIO_MODE_OUTPUT, // pinos para saída
+            .pull_up_en = GPIO_PULLUP_DISABLE,
+            .pull_down_en = GPIO_PULLDOWN_DISABLE,
+            .intr_type = GPIO_INTR_DISABLE // interrupções desabilitadas
+        };
+
+        gpio_config(&gpioConfig);
+    }
+
+public:
     bool motorInvertido = false;
 
     /**
@@ -40,28 +127,14 @@ class A3941 {
      * @param frequenciaDoSinalDePWM Frequência do sinal PWM;
      * @param resolucao Número de bits da resolução do sinal PWM. Por exemplo, uma resolução de 12 bits significa que o valor de PWM varia no intervalo [0, 4095] (2¹² = 4096 níveis de controle).
      */
-    A3941(uint8_t pinoPWML, uint8_t pinoPWMH, uint8_t pinoPHASE, uint8_t canalDoPinoPWMH, uint32_t frequenciaDoSinalDePWM, uint8_t resolucao) :
+    A3941(uint8_t pinoPWML, uint8_t pinoPWMH, uint8_t pinoPHASE, uint8_t canalDoPinoPWMH) :
         // Atribui os valores passados nos atributos
-        _pinoPHASE(pinoPHASE),
-        _canalDoPinoPWMH(canalDoPinoPWMH),
-        _resolucao(resolucao)
+        _pinoPWML(static_cast<gpio_num_t>(pinoPWML)),
+        _pinoPWMH(static_cast<gpio_num_t>(pinoPWMH)),
+        _pinoPHASE(static_cast<gpio_num_t>(pinoPHASE))
     {
-        // Configura o canal que gera o sinal PWM
-        ledcSetup(_canalDoPinoPWMH, frequenciaDoSinalDePWM, resolucao);
-
-        // Anexa o canal que gera o sinal PWM no pino PWMH do driver de motor
-        ledcAttachPin(pinoPWMH, _canalDoPinoPWMH);
-
-        // Define o modo OUTPUT para os pinos PWML e PHASE dos driver de motor
-        // Esses pinos não recebem sinais de PWM
-        pinMode(pinoPWML, OUTPUT);
-        pinMode(_pinoPHASE, OUTPUT);
-
-        // Coloca o pino PWML do driver de motor com valor HIGH
-        digitalWrite(pinoPWML, HIGH);
-
-        // Para o motor
-        parar();
+        setCanalPWM(canalDoPinoPWMH);
+        setResolucao(0); // Para usar a resolução padrão da biblioteca, que é 8 bits
     }
 
     /**
@@ -70,6 +143,8 @@ class A3941 {
      * @param potencia Valor da potência, na qual varia no intervalo [-valorMaximoDePotencia, valorMaximoDePotencia], para aplicar no motor. Para valores positivos o motor gira em um sentido, para valores negativos o motor gira no sentido contrário.
      */
     void setPotencia(int potencia) {
+        potencia = (potencia > (int)getValorMaximoDePotencia()) ? getValorMaximoDePotencia() : potencia; // Limita a potência ao valor máximo permitido
+        potencia = (potencia < -(int)getValorMaximoDePotencia()) ? -getValorMaximoDePotencia() : potencia; // Limita a potência ao valor mínimo permitido
 
         // Se o motor estiver invertido, inverte o valor da potência
         if (motorInvertido) {
@@ -78,30 +153,26 @@ class A3941 {
 
         // Se a potência passada for positiva, escreve esse valor no canal do pino PWMH e escreve LOW no pino PHASE do driver de motor
         if (potencia > 0) {
-            ledcWrite(_canalDoPinoPWMH, potencia);
-            digitalWrite(_pinoPHASE, LOW);
+            ledc_set_duty(modoPWM, _canalDoPinoPWMH, potencia);
+            ledc_update_duty(modoPWM, _canalDoPinoPWMH);
+            gpio_set_level(_pinoPHASE, 0);
         }
         // Senão, se a potência passada for negativa, escreve o valor absoluto desse valor no canal do pino PWMH e escreve HIGH no pino PHASE do driver de motor
         else if (potencia < 0) {
-            ledcWrite(_canalDoPinoPWMH, abs(potencia));
-            digitalWrite(_pinoPHASE, HIGH);
+            ledc_set_duty(modoPWM, _canalDoPinoPWMH, -potencia);
+            ledc_update_duty(modoPWM, _canalDoPinoPWMH);
+            gpio_set_level(_pinoPHASE, 1);
         }
         // Senão, se o valor de potência for 0, freia o motor escrevendo o valor 0 no canal do pino PWMH
         else {
-            ledcWrite(_canalDoPinoPWMH, 0);
+            ledc_stop(modoPWM, _canalDoPinoPWMH, 0);
         }
-
-        // Atualiza o atributo da potência do motor com o valor da potência atual
-        _potencia = potencia;
     }
 
-    // Método para parar o motor
+    /// @brief Método para parar o motor com potência 0
     void parar() {
         // Freia o motor escrevendo o valor 0 no canal do pino PWMH
-        ledcWrite(_canalDoPinoPWMH, 0);
-
-        // Atualiza o atributo da potência do motor com o valor de potência 0
-        _potencia = 0;
+        ledc_stop(modoPWM, _canalDoPinoPWMH, 0);
     }
 
     /**
@@ -110,7 +181,7 @@ class A3941 {
      * @return O valor máximo de potência do motor.
      */
     uint32_t getValorMaximoDePotencia() const {
-        return _valorMaximoDePotencia;
+        return ((1 << resolucao) - 1);
     }
 
     /**
@@ -118,11 +189,37 @@ class A3941 {
      * 
      * @return O valor da potência atual do motor.
      */
-    int getPotencia() const {
-        return _potencia;
+    uint32_t getPotencia() const {
+        return ledc_get_duty(modoPWM, _canalDoPinoPWMH);
     }
 
     void setCanal(uint8_t novoCanalDoPinoPWMH){
-        _canalDoPinoPWMH = novoCanalDoPinoPWMH;
+        _canalDoPinoPWMH = static_cast<ledc_channel_t>(novoCanalDoPinoPWMH);
+    }
+
+    /**
+     * @brief Método para alterar a resolução do PWM
+     * 
+     * @param novaResolucao Número de bits da resolução do sinal PWM valor entre [1, 15]
+     */
+    void setResolucao(uint8_t novaResolucao) {
+        if (novaResolucao > 0 && novaResolucao <= LEDC_TIMER_BIT_MAX) {
+            resolucao = static_cast<ledc_timer_bit_t>(novaResolucao); // converte o valor int passado para a macro do enum ledc_timer_bit_t
+        }
+
+        else {
+            resolucao = ConstA3941::RESOLUCAO_SINAL_PWM; // para qualquer valor fora das resoluções aceita, usará o padrão da biblioteca (8)
+        }
+    }
+
+    /// @brief Método para inicializar o controle do motor, deve ser chamado no setup()
+    void begin() {
+        // Funções para inicializar o canal de PWM e os pinos GPIO do driver de motor
+        initCanalPWM();
+        initGPIO();
+
+        gpio_set_level(_pinoPWML, 1); // Ativa o driver de motor escrevendo HIGH no pino PWML
+
+        parar(); // Garante que o motor comece parado
     }
 };
